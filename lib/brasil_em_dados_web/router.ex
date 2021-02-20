1ffeff00d54a1287c732469ebb4487fb7e2b7354
rogervezaro <rogervezaro1997@gmail.com>
2021-02-20 15:49:43 -0400

    feat: Add Admin panel page

--- lib/brasil_em_dados_web/router.ex
+++ lib/brasil_em_dados_web/router.ex
@@ -17,12 +17,6 @@ defmodule BrasilEmDadosWeb.Router do
     plug :accepts, ["json"]
   end
 
-  scope "/", BrasilEmDadosWeb do
-    pipe_through :browser
-
-    live "/", PageLive, :index
-  end
-
   # Other scopes may use custom stacks.
   # scope "/api", BrasilEmDadosWeb do
   #   pipe_through :api
@@ -44,36 +38,47 @@ defmodule BrasilEmDadosWeb.Router do
     end
   end
 
-  ## Authentication routes
-
+  ## Require not authenticated
   scope "/", BrasilEmDadosWeb do
     pipe_through [:browser, :redirect_if_user_is_authenticated]
 
-    get "/users/register", UserRegistrationController, :new
-    post "/users/register", UserRegistrationController, :create
-    get "/users/log_in", UserSessionController, :new
-    post "/users/log_in", UserSessionController, :create
-    get "/users/reset_password", UserResetPasswordController, :new
-    post "/users/reset_password", UserResetPasswordController, :create
-    get "/users/reset_password/:token", UserResetPasswordController, :edit
-    put "/users/reset_password/:token", UserResetPasswordController, :update
+    scope "/admin" do
+      get "/register", UserRegistrationController, :new
+      post "/register", UserRegistrationController, :create
+      get "/log_in", UserSessionController, :new
+      post "/log_in", UserSessionController, :create
+      get "/reset_password", UserResetPasswordController, :new
+      post "/reset_password", UserResetPasswordController, :create
+      get "/reset_password/:token", UserResetPasswordController, :edit
+      put "/reset_password/:token", UserResetPasswordController, :update
+    end
   end
 
+  # Require authenticated
   scope "/", BrasilEmDadosWeb do
     pipe_through [:browser, :require_authenticated_user]
 
-    get "/users/settings", UserSettingsController, :edit
-    put "/users/settings/update_password", UserSettingsController, :update_password
-    put "/users/settings/update_email", UserSettingsController, :update_email
-    get "/users/settings/confirm_email/:token", UserSettingsController, :confirm_email
+    scope "/admin" do
+
+      live "/", AdminLive.Index, :index
+
+      get "/settings", UserSettingsController, :edit
+      put "/settings/update_password", UserSettingsController, :update_password
+      put "/settings/update_email", UserSettingsController, :update_email
+      get "/settings/confirm_email/:token", UserSettingsController, :confirm_email
+    end
   end
 
   scope "/", BrasilEmDadosWeb do
     pipe_through [:browser]
 
-    delete "/users/log_out", UserSessionController, :delete
-    get "/users/confirm", UserConfirmationController, :new
-    post "/users/confirm", UserConfirmationController, :create
-    get "/users/confirm/:token", UserConfirmationController, :confirm
+    live "/", PageLive, :index
+
+    scope "/admin" do
+      delete "/log_out", UserSessionController, :delete
+      get "/confirm", UserConfirmationController, :new
+      post "/confirm", UserConfirmationController, :create
+      get "/confirm/:token", UserConfirmationController, :confirm
+    end
   end
 end
