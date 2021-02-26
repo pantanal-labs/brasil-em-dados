cd10d54ad7e7cf9e3ff7de3d0ddda930f6d23309
rogervezaro <rogervezaro1997@gmail.com>
2021-02-26 15:32:03 -0400

    feat: We can now delete posts

--- lib/brasil_em_dados_web/live/admin/posts_live/index.ex
+++ lib/brasil_em_dados_web/live/admin/posts_live/index.ex
@@ -20,11 +20,33 @@ defmodule BrasilEmDadosWeb.Admin.PostsLive.Index do
     {:noreply, assign(socket, posts: posts, options: paginate_options)}
   end
 
+  @impl true
+  def handle_event("delete", %{"id" => id}, socket) do
+    post = Blog.get_post!(id)
+    delete_post(post, socket)
+  end
+
   defp pagination_link(socket, text, page, limit, class) do
     live_patch(text,
-      to: Routes.posts_index_path(socket, :index, page: page,
-        limit: limit),
+      to:
+        Routes.posts_index_path(socket, :index,
+          page: page,
+          limit: limit
+        ),
       class: class
     )
+  end
+
+  defp delete_post(post, socket) do
+    case Blog.delete_post(post) do
+      {:ok, _} ->
+        {:noreply,
+         socket
+         |> put_flash(:info, "Post deletado com sucesso!")
+         |> redirect(to: Routes.posts_index_path(socket, :index))}
+
+      {:error, _} ->
+        {:noreply, socket |> put_flash(:error, "Error!!!")}
+    end
   end
 end
